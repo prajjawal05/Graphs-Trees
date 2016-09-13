d0d1add27a2fe4b86d2d3b717ff9380a7eaefb08
Prajjawal Agarwal <prajjawalagarwal@gmail.com>
2016-09-13 14:08:04 +0530

    Create Range update SEGTREE.cpp

---
+++ Range update SEGTREE.cpp
@@ -0,0 +1,30 @@
+/* if(tree[pos]==1)
+    return;
+    
+    
+    take y at all place instead of input      //Tripathihelp
+    
+    a[node] ==1
+    
+scanf() <-- cin */   
+    
+void update(int segtree[],int y[],int low,int high,int pos,int qlow,int qhigh)
+{
+    if(segtree[pos]==1)
+        return;
+    if(qlow>high || qhigh<low)
+        return;
+    if(low==high)
+    {
+        y[low]=y[low]/a[y[low]];    /* a stores the lowestprime */ 
+        segtree[pos]=a[y[low]];             /*input --> original array*/
+        return;
+    }
+    else
+    {
+            int mid = (low+high)/2;
+        update(segtree,y,low,mid,2*pos+1,qlow,qhigh);
+        update(segtree,y,mid+1,high,2*pos+2,qlow,qhigh);
+        segtree[pos] = max(segtree[2*pos + 1],segtree[2*pos + 2]);
+    }
+}    
